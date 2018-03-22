0170a4f0a11b63d1b3261ae96568212e8a0b1ae9
Victor Petukhov <i@victor.am>
2018-03-23 00:03:07 +0300

    Initial commit

---
+++ src/main/kotlin/org/jetbrains/bytecodetosourcemapper/main.kt
@@ -0,0 +1,11 @@
+package org.jetbrains.bytecodetosourcemapper
+
+import com.xenomachina.argparser.ArgParser
+
+fun main(args : Array<String>) {
+    val parser = ArgParser(args)
+    val classesDirectory by parser.storing("--classes_directory", help="path to folder with class files (with JVM-bytecode)")
+    val sourcesDirectory by parser.storing("--sources_directory", help="path to folder with Kotlin source code files")
+
+//    Runner.run(classesDirectory, sourcesDirectory)
+}
